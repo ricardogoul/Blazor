3bbe8fbcff23b21f023c6a29c9ce1bdb2d03cf9f
Ricardo de Oliveira Goulart <ricardogoul.si@gmail.com>
2024-05-07 17:19:40 -0400

    import msc.sqlclient e criada conexao com o banco

---
+++ ScreenSound/Banco/Connection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ScreenSound.Banco
+{
+    internal class Connection
+    {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=FalseData Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        public SqlConnection ObterConexao()
+        {
+            return new SqlConnection(connectionString); 
+        }        
+    }
+}
