d61d4cc6da4dc3efc1c31f7815876dd86e7da1ad
Ricardo Goulart <ricardogoul.si@gmail.com>
2024-08-14 14:50:40 -0400

    Refatoração do projeto e adição do swagger

---
+++ ScreenSound.API/Endpoints/ArtistasExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.API.Endpoints
+{
+    public static class ArtistasExtensions
+    {
+        public static void AddEndpointsArtistas(this WebApplication app)
+        {
+            app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
+            {
+                return Results.Ok(dal.Listar());
+            });
+
+            app.MapGet("/Artistas/{nome}", ([FromServices] DAL<Artista> dal, string nome) =>
+            {
+                var artista = dal.RecuperarPor(artista => artista.Nome.ToUpper().Equals(nome.ToUpper()));
+                if (artista is null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(artista);
+            });
+
+            app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) =>
+            {
+                dal.Adicionar(artista);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
+            {
+                var artista = dal.RecuperarPor(artista => artista.Id.Equals(id));
+                if (artista is null)
+                {
+                    return Results.NotFound();
+                }
+
+                dal.Deletar(artista);
+                return Results.Ok();
+            });
+
+            app.MapPatch("/Artistas/{id}", ([FromServices] DAL<Artista> dal, [FromBody] Artista artistaEditado, int id) =>
+            {
+                var artista = dal.RecuperarPor(artista => artista.Id.Equals(id));
+                if (artista is null)
+                {
+                    return Results.NotFound();
+                }
+
+                artista.Nome = artistaEditado.Nome != "" ? artistaEditado.Nome : artista.Nome;
+                artista.Bio = artistaEditado.Bio != "" ? artistaEditado.Bio : artista.Bio;
+                artista.FotoPerfil = artistaEditado.FotoPerfil != "" ? artistaEditado.FotoPerfil : artista.FotoPerfil;
+
+                dal.Atualizar(artista);
+                return Results.Ok();
+            });
+        }
+    }
+}
--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ScreenSound.API.Endpoints;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 using System.Data.SqlTypes;
@@ -9,101 +10,15 @@ builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =
 builder.Services.AddDbContext<ScreenSoundContext>();
 builder.Services.AddTransient<DAL<Artista>>();
 builder.Services.AddTransient<DAL<Musica>>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) => 
-{
-    return Results.Ok(dal.Listar());
-});
+app.AddEndpointsArtistas();
+app.AddEndpointsMusicas();
 
-app.MapGet("/Artistas/{nome}", ([FromServices] DAL<Artista> dal, string nome) =>
-{
-    var artista =  dal.RecuperarPor(artista => artista.Nome.ToUpper().Equals(nome.ToUpper()));
-    if(artista is null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(artista);
-});
-
-app.MapPost("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody]Artista artista) => 
-{
-    dal.Adicionar(artista);
-    return Results.Ok();
-});
-
-app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
-{
-    var artista = dal.RecuperarPor(artista => artista.Id.Equals(id));
-    if (artista is null)
-    {
-        return Results.NotFound();
-    }
-
-    dal.Deletar(artista);
-    return Results.Ok();
-});
-
-app.MapPatch("/Artistas/{id}", ([FromServices] DAL<Artista> dal, [FromBody]Artista artistaEditado, int id) =>
-{
-    var artista = dal.RecuperarPor(artista => artista.Id.Equals(id));
-    if (artista is null)
-    {
-        return Results.NotFound();
-    }
-
-    artista.Nome = artistaEditado.Nome != "" ? artistaEditado.Nome : artista.Nome;
-    artista.Bio = artistaEditado.Bio != "" ? artistaEditado.Bio : artista.Bio;
-    artista.FotoPerfil = artistaEditado.FotoPerfil != "" ? artistaEditado.FotoPerfil : artista.FotoPerfil;
-
-    dal.Atualizar(artista);
-    return Results.Ok();
-});
-
-app.MapGet("/Musicas", ([FromServices] DAL<Musica> dal) =>
-{
-    return Results.Ok(dal.Listar());
-});
-
-app.MapGet("/Musicas/{nome}", ([FromServices] DAL<Musica> dal, string nome) =>
-{
-    var musica = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
-    if (musica is null)
-    {
-        return Results.NotFound();
-    }
-    return Results.Ok(musica);
-
-});
-
-app.MapPost("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) =>
-{
-    dal.Adicionar(musica);
-    return Results.Ok();
-});
-
-app.MapDelete("/Musicas/{id}", ([FromServices] DAL<Musica> dal, int id) => {
-    var musica = dal.RecuperarPor(a => a.Id == id);
-    if (musica is null)
-    {
-        return Results.NotFound();
-    }
-    dal.Deletar(musica);
-    return Results.NoContent();
-
-});
-
-app.MapPut("/Musicas", ([FromServices] DAL<Musica> dal, [FromBody] Musica musica) => {
-    var musicaAAtualizar = dal.RecuperarPor(a => a.Id == musica.Id);
-    if (musicaAAtualizar is null)
-    {
-        return Results.NotFound();
-    }
-    musicaAAtualizar.Nome = musica.Nome;
-    musicaAAtualizar.AnoLancamento = musica.AnoLancamento;
-
-    dal.Atualizar(musicaAAtualizar);
-    return Results.Ok();
-});
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
